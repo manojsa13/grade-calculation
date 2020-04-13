79e9897de65f37b6364ae6ed28dff8a371a12178
manojsa13 <63357275+manojsa13@users.noreply.github.com>
2020-04-13 17:58:07 +0530

    checking the eligibility for voting by age
    Using a conditional statement to check the eligibility, if the age is greater than or equal to 18 is eligible else not eligible.

---
+++ vote.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int age;
+	cout<<"ENTER THE AGE OF THE USER :: ";
+	cin>>age;
+	if(age>=18)
+	cout<<"ELIGIBLE FOR VOTING";
+	else
+	cout<<"NOT ELIGIBLE FOR VOTING ";
+	return 0;
+}
